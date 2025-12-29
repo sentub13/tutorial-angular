8aa25aa48cd6b0038c1134aa7e0e09390eb9a986
sentub13 <sentu.biswas@ust.com>
2025-12-29 17:31:58 +0530

    asda

--- demo01/src/app/todo/todo.component.ts
+++ demo01/src/app/todo/todo.component.ts
@@ -6,11 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
+  isEdit = false;
   todoForm = {
-    id: null,
+    id: 0,
     name: '',
     email: '',
-    message: ''
+    message: '',
+    completed: false
   };
 
   todos = [
@@ -18,17 +20,27 @@ export class TodoComponent {
     { id: 2, name: 'Learn React', email: 'kjb@gmail.com', message: 'Welcome', completed: false },
   ];
 
-  newTodo = '';
+
+  isFormValid(): boolean {
+    const { name, email, message } = this.todoForm;
+    return !!(name?.trim() && email?.trim() && message?.trim());
+  }
+
+  resetForm() {
+    this.todoForm = { id: 0, name: '', email: '', message: '',  completed: false };
+  }
 
   addTodo() {
     console.log("todoForm----", this.todoForm)
-    if (this.newTodo.trim()) {
-      // this.todos.push({
-      //   id: Date.now(),
-      //   text: this.newTodo.trim(),
-      //   completed: false
-      // });
-      // this.newTodo = '';
+    if (this.isFormValid()) {
+      this.todos.push({
+        id: Math.round(Math.random()),
+        name: this.todoForm.name.trim(),
+        email: this.todoForm.email.trim(),
+        message: this.todoForm.message.trim(),
+        completed: false
+      });     
+      this.resetForm();
     }
   }
 
@@ -36,7 +48,21 @@ export class TodoComponent {
     todo.completed = !todo.completed;
   }
 
-  deleteTodo(id: number) {
-    this.todos = this.todos.filter(todo => todo.id !== id);
+  editTodo (index:number) {
+    this.isEdit = true;
+    let selected = this.todos[index];
+    console.log("selected----", selected);
+    this.todoForm = {
+      id: selected.id,
+      name: selected.name,
+      email: selected.email,
+      message: selected.message,
+      completed: selected.completed
+    };
+  }
+
+  deleteTodo(index: number) {
+    // deleteTodo.todos = this.todos.filter(todo => todo.id !== id);
+    this.todos.splice(index, 1);
   }
 }
