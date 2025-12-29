e0b8fd6f494eb082739ea70e6dd8dbb9d473f593
sentub13 <sentu.biswas@ust.com>
2025-12-29 16:42:41 +0530

    sadsa

--- demo01/src/app/todo/todo.component.ts
+++ demo01/src/app/todo/todo.component.ts
@@ -6,21 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
+  todoForm = {
+    id: null,
+    name: '',
+    email: '',
+    message: ''
+  };
+
   todos = [
-    { id: 1, text: 'Learn Angular', completed: false },
-    { id: 2, text: 'Build Todo App', completed: false }
+    { id: 1, name: 'Learn Angular', email: 'abc@gmail.com', message: 'World', completed: false },
+    { id: 2, name: 'Learn React', email: 'kjb@gmail.com', message: 'Welcome', completed: false },
   ];
 
   newTodo = '';
 
   addTodo() {
+    console.log("todoForm----", this.todoForm)
     if (this.newTodo.trim()) {
-      this.todos.push({
-        id: Date.now(),
-        text: this.newTodo.trim(),
-        completed: false
-      });
-      this.newTodo = '';
+      // this.todos.push({
+      //   id: Date.now(),
+      //   text: this.newTodo.trim(),
+      //   completed: false
+      // });
+      // this.newTodo = '';
     }
   }
 
